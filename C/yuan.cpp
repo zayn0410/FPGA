bbfd0c51610ff0a8ebd81d7a307b39d5793e8cfa
ZAYN0410 <513933110@qq,com>
2025-03-19 21:48:22 +0800

    此文件夹全部提交

---
+++ C/yuan.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+{
+	int s,n;
+	s= 2147483647;
+	n=s+1;
+	printf("nµÄÖµÎª£º%d\n",sizeof(n));
+	return 0;
+}   
